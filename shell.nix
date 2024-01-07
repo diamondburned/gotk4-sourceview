fcec82221ecd90ff3c221430b11bbfd987437db1
diamondburned <diamond@arikawa-hi.me>
2024-01-07 13:45:14 -0800

    Update shell.nix to newer Nixpkgs and gotk4-nix

--- shell.nix
+++ shell.nix
@@ -1,31 +1,33 @@
-let systemPkgs = import <nixpkgs> {};
+{
+	fetchFromGitHub ? (import <nixpkgs> {}).fetchFromGitHub,
 
-in {
-	gotk4 ? (systemPkgs.fetchFromGitHub {
-		owner = "diamondburned";
-		repo  = "gotk4";
-		rev   = "2b51f7b62";
-		hash  = "sha256:0lcwqkdqqkqsihzashyn3vw8mk6my0f3msxsjw7xsdk9zdv6frpf";
-	}),
+	gotk4-nix ? fetchFromGitHub {
+		owner  = "diamondburned";
+		repo   = "gotk4-nix";
+		rev    = "ad91dabf706946c4380d0a105f0937e4e8ffd75f";
+		sha256 = "0rkw9k98qy7ifwypkh2fqhdn7y2qphy2f8xjisj0cyp5pjja62im";
+	},
 
-	pkgs ? (import "${gotk4}/.nix/pkgs.nix" {
-		src = systemPkgs.fetchFromGitHub {
-			owner = "NixOS";
-			repo  = "nixpkgs";
-			rev   = "3fdd780";
-			hash  = "sha256:0df9v2snlk9ag7jnmxiv31pzhd0rqx2h3kzpsxpj07xns8k8dghz";
-		};
-	}),
+	nixpkgs ? fetchFromGitHub {
+		owner = "NixOS";
+		repo  = "nixpkgs";
+		rev   = "70bdadeb94ffc8806c0570eb5c2695ad29f0e421"; # nixos-23.05
+		hash  = "sha256-LWvKHp7kGxk/GEtlrGYV68qIvPHkU9iToomNFGagixU=";
+	},
 }:
 
-let shell = import "${gotk4}/.nix/shell.nix" {
-	inherit pkgs;
-};
-
-in shell.overrideAttrs (old: {
-	buildInputs = old.buildInputs ++ (with pkgs; [
-		gtksourceview3
-		gtksourceview4
-		gtksourceview5
-	]);
-})
+import "${gotk4-nix}/shell.nix" {
+	base = {
+		pname = "gotk4-sourceview";
+		version = "dev";
+		buildInputs = pkgs: with pkgs; [
+			gtksourceview3
+			gtksourceview4
+			gtksourceview5
+		];
+	};
+	pkgs = import "${gotk4-nix}/pkgs.nix" {
+		sourceNixpkgs = nixpkgs;
+		useFetched = true;
+	};
+}
